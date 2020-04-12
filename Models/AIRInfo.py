855bb1a0c2ae2bcc7e4325cef4a647282a1c902a
afonsobm <bruno.ma@poli.ufrj.br>
2020-04-12 17:07:42 -0300

    -- first incomplete version

--- Models/AIRInfo.py
+++ Models/AIRInfo.py
@@ -1,11 +1,65 @@
 class AIRInfo:
+    angle: str
+    azimuth_angle: str
+    cal_plate: str
+    channel: int
+    contact: str
+    contact_pressure: str
+    d_height: str
+    d_mic_mic: str
+    d_speaker_mic: str
     description: str
-    frequency: int
-    name: str
+    distance: int
+    erp_dist: str
+    excitation: str
+    filename: str
+    fs: int
+    head: int
+    hhp_xyz: str
+    mic_height: str
+    microphone: str
+    misc: str
+    mock_up_type: str
+    mrp_height: str
+    phone_pos: str
+    pinna: str
+    precision: str
+    rir_method: str
+    rir_type: str
+    room: str
+    torso: str
+    version: str
 
-    def __init__(self, description: str, frequency: int, name: str) -> None:
+    def __init__(self, angle: str, azimuth_angle: str, cal_plate: str, channel: int, contact: str, contact_pressure: str, copyright: str, d_height: str, d_mic_mic: str, d_speaker_mic: str, description: str, distance: int, erp_dist: str, excitation: str, filename: str, fs: int, head: int, hhp_xyz: str, mic_height: str, microphone: str, misc: str, mock_up_type: str, mrp_height: str, phone_pos: str, pinna: str, precision: str, rir_method: str, rir_type: str, room: str, torso: str, version: str) -> None:
+        self.angle = angle
+        self.azimuth_angle = azimuth_angle
+        self.cal_plate = cal_plate
+        self.channel = channel
+        self.contact = contact
+        self.contact_pressure = contact_pressure
+        self.d_height = d_height
+        self.d_mic_mic = d_mic_mic
+        self.d_speaker_mic = d_speaker_mic
         self.description = description
-        self.frequency = frequency
-        self.name = name
+        self.distance = distance
+        self.erp_dist = erp_dist
+        self.excitation = excitation
+        self.filename = filename
+        self.fs = fs
+        self.head = head
+        self.hhp_xyz = hhp_xyz
+        self.mic_height = mic_height
+        self.microphone = microphone
+        self.misc = misc
+        self.mock_up_type = mock_up_type
+        self.mrp_height = mrp_height
+        self.phone_pos = phone_pos
+        self.pinna = pinna
+        self.precision = precision
+        self.rir_method = rir_method
+        self.rir_type = rir_type
+        self.room = room
+        self.torso = torso
+        self.version = version
 
-    __init__.__defaults__ = tuple(None for name in __init__.__code__.co_varnames)
+        __init__.__defaults__ = tuple(None for name in __init__.__code__.co_varnames)
--- Util/LoadAIR.py
+++ Util/LoadAIR.py
@@ -1,9 +1,45 @@
 import os
+
+import numpy as np
 from scipy.io import loadmat
+
+from Models.AIR import AIR
+from Models.AIRInfo import AIRInfo
 
 airLibPath = os.path.normpath(os.getenv("AIR_LIB_PATH"))
 
-def loadAIR(fileName: str):
+def loadAIR(fileName: str) -> AIR:
     
     airFile = loadmat(os.path.join(airLibPath, fileName))
-    return airFile
+    airFileInfo = airFile.get("air_info")
+
+    infoValues = airFileInfo[0,0]
+    infoNames = airFileInfo[0,0].dtype.names
+
+    air = AIR(airFile.get("h_air")[0], generateAIRInfo(infoValues, infoNames))
+
+    return air
+
+def generateAIRInfo(infoValues: np.ndarray, infoNames: tuple) -> AIRInfo:
+    
+    airInfo = AIRInfo()
+
+    airInfo.description = infoValues[infoNames.index("description")][0]
+    airInfo.precision = infoValues[infoNames.index("precision")][0]
+    airInfo.room = infoValues[infoNames.index("room")][0]
+    airInfo.head = infoValues[infoNames.index("head")][0][0]
+    airInfo.distance = infoValues[infoNames.index("distance")][0][0]
+    airInfo.fs = infoValues[infoNames.index("fs")][0][0]
+    airInfo.channel = infoValues[infoNames.index("channel")][0][0]
+    airInfo.filename = infoValues[infoNames.index("filename")][0]
+    
+    # airInfo.description = infoValues[infoNames.index("description")][0]
+    # airInfo.precision = infoValues[infoNames.index("precision")][0]
+    # airInfo.room = infoValues[infoNames.index("room")][0]
+    # airInfo.head = infoValues[infoNames.index("head")][0][0]
+    # airInfo.distance = infoValues[infoNames.index("distance")][0][0]
+    # airInfo.fs = infoValues[infoNames.index("fs")][0][0]
+    # airInfo.channel = infoValues[infoNames.index("channel")][0][0]
+    # airInfo.filename = infoValues[infoNames.index("filename")][0]
+
+    return airInfo
