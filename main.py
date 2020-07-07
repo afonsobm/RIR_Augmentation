60a40560b72e999db37a213b82a6ee9a56c95ef0
Bruno <bruno.ma0738gm@gmail.com>
2020-07-07 20:36:00 -0300

    using txt file

--- main.py
+++ main.py
@@ -5,10 +5,13 @@ from scipy.io.matlab import loadmat
 
 from Models.AIR import AIR
 from Models.AIRInfo import AIRInfo
-from Util.LoadAIR import loadAIR
+from Util.LoadAIR import load_air
 
 if __name__ == "__main__" :
 
-    fileName = 'air_binaural_lecture_0_0_1.mat'
-    airFile = loadAIR(fileName)
-    pass
+    for fileName in open('load_air_files.txt', 'r'):
+        
+        fileName = fileName.rstrip()
+        print(fileName)
+        airFile = load_air(fileName)
+        pass
