babd60432090ee56d69feffd2cae99e999b259f1
Bruno <bruno.ma0738gm@gmail.com>
2020-07-09 14:17:10 -0300

    adding code description on main

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
+# =======================================================
+# GRADUATION PROJECT - ROOM IMPULSE RESPONSE AUGMENTATION
+# =======================================================
+
+# ================
+# AUTHOR - VERSION
+# ================
+
+#   Author: Bruno Machado Afonso
+#   Version: 1.0
+#   Institution: Escola Politecnica - Universidade Federal do Rio de Janeiro (UFRJ), Rio de Janeiro, Brazil
+
+# ===========
+# DESCRIPTION
+# ===========
+
+#   This project is based on the implementation of the work presented on the following articles
+        
+        # 1) IMPULSE RESPONSE DATA AUGMENTATION AND DEEP NEURAL NETWORKS FOR BLIND ROOM ACOUSTIC PARAMETER ESTIMATION - Nicholas J. Bryan - Adobe Research, San Francisco, CA, USA
+        # 2) A STUDY ON DATA AUGMENTATION OF REVERBERANT SPEECH FOR ROBUST SPEECH RECOGNITION - Tom Ko et al. - Huawei Noah’s Ark Research Lab, Hong Kong, China
+
+#   The main objective is to create a method which generates artificial Room Impulse Response (RIR) by modifying the
+#   Direct-to-Reverberant Ratio (DRR) and Reverberation Time (T60) parameters extracted from the RIR
+#   Later, these can be aplied to speech signals to generate far-field speechs (using augmented RIR and artificial noises)
+
+
 import os
 
 import numpy as np
